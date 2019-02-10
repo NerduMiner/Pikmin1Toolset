b466dcd6bb187a1f00c0ae3d91d2d6093ffa8e2a
Unknown <keefer.ian@gmail.com>
2019-02-10 11:51:55 -0500

    Add Dependencies

---
+++ libs/ambroStuf.py
@@ -0,0 +1,186 @@
+'''// Collision info file for model.mod
+collinfo 0 {		// kosijnt
+	id		boun
+	code		none
+	type	1
+	radius	33.333336
+	centre	19.000000 -14.000000 0.000000
+
+	collinfo 17 {		// centnull
+		id		carc
+		code		none
+		type	1
+		radius	0.000000
+		centre	0.000000 0.000000 0.000000
+		}
+
+	collinfo 7 {		// headjnt
+		id		cent
+		code		st**
+		type	1
+		radius	16.000000
+		centre	0.000000 -14.000000 0.000000
+		}
+	}
+
+collinfo 0 {		// kosijnt
+	id		plat
+	code		s
+	type	2
+	platform	tekis/frog/frogcol.mod
+	}
+
+collinfo 6 {		// lasijnt3
+	id		lleg
+	code		none
+	type	1
+	radius	10.000000
+	centre	0.000000 0.000000 0.000000
+	}
+
+collinfo 3 {		// rasijnt3
+	id		rleg
+	code		none
+	type	1
+	radius	10.000000
+	centre	0.000000 0.000000 0.000000
+	}
+'''
+import os
+class CmdStream():    
+    def getToken(line, getTokenIndex):
+        if not (line and line.strip()): # if whitespace
+            return ""
+        #if hasComments:
+         #   line.split("//", 1)
+        try:
+            if line:
+                return line.split()[getTokenIndex]
+        except IndexError:
+            pass
+
+class ObjCollInfo():
+    def loadini(fContents):
+        for line in fContents:
+            if not line.find("{") == -1: # if closed bracket
+                print("opening bracket found!")
+                print("name of collinfo part : " + CmdStream.getToken(line, -1))
+
+            if not line.find("id") == -1:
+                print("id : " + CmdStream.getToken(line, 1))
+
+            if not line.find("code") == -1:
+                print("code : " + CmdStream.getToken(line, 1))
+
+            if not line.find("type") == -1:
+                print("type : " + CmdStream.getToken (line,1))
+
+            if not line.find("platform") == -1:
+                print("platform : " + CmdStream.getToken(line, 1))
+
+            if not line.find("radius") == -1:
+                print("radius : " + CmdStream.getToken(line, 1))
+
+            if not line.find("getminy") == -1:
+                print("get minimum y : " + CmdStream.getToken(line, 1))
+
+            if not line.find("collinfo") == -1:
+                print("REPEATING COLLINFO FUNC!!!")
+                
+            if not line.find("}") == -1: # if closed bracket
+                print("closed bracket found!\n\n")
+                return
+
+class LightGroup():
+    def loadini(fContents):
+        for line in fContents:
+            if not line.find("{") == -1: # if closed bracket
+                print("opening bracket found!")
+                print("name of lightgroup part : " + CmdStream.getToken(line, -1))
+
+            if not line.find("type") == -1:
+                print("type : " + CmdStream.getToken (line,1))
+
+            if not line.find("flags") == -1:
+                print("flags : " + CmdStream.getToken(line, 1))
+
+            if not line.find("colour") == -1:
+                print("colour : " + CmdStream.getToken(line, 1) + CmdStream.getToken(line, 2) + CmdStream.getToken(line, 3) + CmdStream.getToken(line, 4))
+
+            if not line.find("texture") == -1:
+                print("texture name : " + CmdStream.getToken(line, 1))
+
+            if not line.find("material") == -1:
+                print("material name : " + CmdStream.getToken(line, 1))
+
+            if not line.find("lightflare") == -1:
+                print("lightflare detected! \n")
+
+            if not line.find("size") == -1:
+                print("size of lightflare : " + CmdStream.getToken(line, 1))
+
+            if not line.find("pos") == -1:
+                print("position of lightflare : " + CmdStream.getToken(line, 1) + CmdStream.getToken(line, 2) + CmdStream.getToken(line, 3))
+                
+            if not line.find("}") == -1: # if closed bracket
+                print("closed bracket found!\n\n")
+                return
+
+class BaseShape():
+    def isolateINISections(f):
+        fContents = []
+        fContentType = []
+        sectionRead = False
+        sectionOpened = 0
+        sectionClosed = 0
+        for line in f: # while (!Cmdstream::endofcmds(v18) && !Cmdstream::endofsection(v18))
+            descriptor = CmdStream.getToken(line, 0) #CmdStream::isToken
+
+            if not (line and line.strip()): # if line is white space 
+                continue # just go to the next iteration
+
+            if not line.find("{") == -1: # if open bracket
+                fContentType.append(CmdStream.getToken(line, 0) + "\n")
+                sectionOpened = sectionOpened + 1
+                sectionRead = True
+
+            if not line.find("}") == -1: # if closed bracket
+                fContents.append(CmdStream.getToken(line, 0) + "\n") # append that line to the array
+                sectionClosed = sectionClosed + 1
+                
+                sectionRead = False
+
+            if sectionRead: # if section has been found
+                fContents.append(line) # append that line to the array
+
+        if sectionOpened == sectionClosed:
+            print(str(sectionOpened) + " sections detected") # make sure we have confirmed amount of sections
+            #print("section content types are:\n"+"".join(fContentType))
+            return fContents, fContentType
+        return fContents, fContentType# return the sections
+    
+    def importIni(f):
+        print("importing ini...")
+        fContents, fContentType = BaseShape.isolateINISections(f)
+        for i in range(len(fContentType)):
+            #print("CONTENT TYPE = " + str(fContentType[i]) + "\n")
+            if not fContentType[i].find("collinfo\n") == -1:
+                print("COLLINFO")
+                ObjCollInfo.loadini(fContents)
+            if not fContentType[i].find("lightgroup\n") == -1:
+                print("LIGHT GROUP!!")
+                LightGroup.loadini(fContents)
+           # if not fContentType[i].find("routes") == -1:
+               # print("routes")'''
+
+f = open("mario.mod.ini")
+
+BaseShape.importIni(f)
+
+
+
+
+
+
+
+
---
+++ libs/gx.py
@@ -0,0 +1,143 @@
+from enum import Enum, IntEnum
+
+VTXFMT = Enum("VTXFMT", ["NOT_PRESENT", "DIRECT", "INDEX8", "INDEX16"], start=0)
+
+components = ["PosMatIdx"]
+
+for i in range(8):
+    components.append("Tex{0}MatIdx".format(i))
+
+components.extend(["Position", "Normal", "Color0", "Color1"])
+
+for i in range(8):
+    components.append("Tex{0}Coord".format(i))
+VTX = IntEnum("VTX", components)
+
+
+def get_vtxformat(val):
+    if val == 0:
+        return VTXFMT.NOT_PRESENT
+    elif val == 1:
+        return VTXFMT.DIRECT
+    elif val == 2:
+        return VTXFMT.INDEX8
+    elif val == 3:
+        return VTXFMT.INDEX16
+    else:
+        raise RuntimeError("unknown format: {0}".format(val))
+
+
+class VertexDescriptor(object):
+    def __init__(self):
+        self.posmat = False
+        self.texmat = [False, False, False, False, False, False, False, False]
+        self.position = VTXFMT.NOT_PRESENT
+        self.normal = VTXFMT.NOT_PRESENT
+        self.color0 = VTXFMT.NOT_PRESENT
+        self.color1 = VTXFMT.NOT_PRESENT
+        self.texcoord = []
+        for i in range(8):
+            self.texcoord.append(VTXFMT.NOT_PRESENT)
+
+    def active_attributes(self):
+        for attr in VTX:
+            if self.exists(attr):
+                if (attr in (VTX.Position, VTX.Normal, VTX.Color0, VTX.Color1) 
+                    or VTX.Tex0Coord <= attr <= VTX.Tex7Coord):
+                    yield (attr, self.get_format(attr))
+                else:
+                    yield (attr, None)
+
+    def exists(self, enumval):
+        if VTX.Tex0MatIdx <= enumval <= VTX.Tex7MatIdx:
+            texmatid = enumval-VTX.Tex0MatIdx
+            return self.texmat[texmatid]
+        elif VTX.Tex0Coord <= enumval <= VTX.Tex7Coord:
+            texcoordid = enumval - VTX.Tex0Coord
+            return self.texcoord[texcoordid] != VTXFMT.NOT_PRESENT
+        elif enumval == VTX.PosMatIdx:
+            return self.posmat
+        elif enumval == VTX.Position:
+            return self.position != VTXFMT.NOT_PRESENT
+        elif enumval == VTX.Normal:
+            return self.normal != VTXFMT.NOT_PRESENT
+        elif enumval == VTX.Color0:
+            return self.color0 != VTXFMT.NOT_PRESENT
+        elif enumval == VTX.Color1:
+            return self.color1 != VTXFMT.NOT_PRESENT
+        else:
+            raise RuntimeError("Unknown enum for exists: {0}".format(str(enumval)))
+
+    def get_format(self, enumval):
+        if enumval == VTX.Position:
+            return self.position
+        elif enumval == VTX.Normal:
+            return self.normal
+        elif enumval == VTX.Color0:
+            return self.color0
+        elif enumval == VTX.Color1:
+            return self.color1
+        elif VTX.Tex0Coord <= enumval <= VTX.Tex7Coord:
+            texcoordid = enumval - VTX.Tex0Coord
+            return self.texcoord[texcoordid]
+        else:
+            raise RuntimeError("Unknown enum for format: {0}", str(enumval))
+
+    def from_value(self, val):
+        #print("test", bin(val))
+        self.posmat = (val & 0b1) == 1
+        val = val >> 1
+
+        for i in range(8):
+            self.texmat[i] = (val & 0b1) == 1
+            val = val >> 1
+
+        self.position = get_vtxformat(val & 0b11)
+        val = val >> 2
+        self.normal = get_vtxformat(val & 0b11)
+        val = val >> 2
+        self.color0 = get_vtxformat(val & 0b11)
+        val = val >> 2
+        self.color1 = get_vtxformat(val & 0b11)
+        val = val >> 2
+
+        for i in range(8):
+            self.texcoord[i] = get_vtxformat(val & 0b11)
+            val = val >> 2
+    
+    def from_pikmin1(self, val, hasNormals=False):
+        self.position = VTXFMT.INDEX16 # Position is implied to be always enabled
+    
+        self.posmat = (val & 0b1) == 1
+        val = val >> 1
+        
+        self.texmat[1] = (val & 0b1) == 1
+        val = val >> 1
+        
+        if (val & 0b1): 
+            self.color0 = VTXFMT.INDEX16
+        val = val >> 1 
+        
+        for i in range(8):
+            if (val & 0b1) == 1:
+                self.texcoord[i] = VTXFMT.INDEX16
+            val = val >> 1
+        
+        if hasNormals:
+            self.normal = VTXFMT.INDEX16
+        
+        
+    
+    def __str__(self):
+        return str([x for x in self.active_attributes()])
+
+if __name__ == "__main__":
+    # pikmin1descriptor 
+    # first bit: posmtx
+    # second bit: tex1mtx
+    # third bit: color0 (if set, color0 is Index16)
+    # 4th-12th bit: tex0 to tex7 coord (if set, texcoord is Index16)
+
+    vcd = VertexDescriptor()
+    vcd.from_pikmin1(0x3, hasNormals=True)
+    print(vcd)
